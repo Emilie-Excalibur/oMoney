6512685518c80f574dd05d291099bae986e9e1a7
Emilie-Excalibur <emiliemaniglier@gmail.com>
2020-04-12 00:30:17 +0200

    Ajout des données du form inscription dans la BDD

--- public/index.php
+++ public/index.php
@@ -8,6 +8,52 @@ require __DIR__ . '/../vendor/autoload.php';
 // Inclu les controllers
 require __DIR__ . '/../app/controllers/MainController.php';
 
+// Inclu les fonctions utiles au site
+require __DIR__ . '/../app/utils/functions.php';
+
+// Inclu la classe Database qui sert à se connect à la BDD
+require __DIR__ . '/../app/utils/Database.php';
+
+/**
+ * DONNEES & BDD
+ */
+
+// Validation des données du formulaire d'inscription
+
+if(!empty($_POST)) {
+    // Récupération des valeurs du formulaire dans des variables
+    $name = isset($_POST['username']) ? $_POST['username'] : '';
+    $email = isset($_POST['email']) ? $_POST['email'] : '';
+    $password = isset($_POST['password']) ? $_POST['password'] : '';
+
+    // Insertion des données dans la BDD
+    
+    // Initialise PDO à partir de la classe Database
+    $pdo = Database::getPDO();
+
+    $insertQuery = "INSERT INTO users (name, email, password)
+    VALUES ('{$name}', '{$email}', '{$password}')";
+
+    $nbInsertedValues = $pdo->exec($insertQuery);
+
+    if($nbInsertedValues === 1) {
+
+        // Si l'insertion s'est bien passée
+        // Redirige vers la page home
+        header('Location:' . $_SERVER['BASE_URI'] . '/');
+        exit;
+
+    } else {
+        echo "Un problème est survenu, merci de réessayer ultérieurement";
+    }
+}
+
+
+/**
+ * GESTION DES ROUTES & AFFICHAGE DES VUES
+ */
+
+
 // Instanciation d'un nouvel objet de la classe AltoRouter
 $router = new AltoRouter();
 
@@ -49,6 +95,7 @@ if($match) {
 }
 
 
+
     // Rendu visuel
     
 // Instanciation de la classe MainController
@@ -56,3 +103,5 @@ $controller = new MainController();
 
 // Appel la méthode correspondante du controller afin d'afficher la page demandée
 $controller->$methodToCall();
+
+
